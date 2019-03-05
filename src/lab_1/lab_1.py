1eeda23b0d76d2f9a603c4d44d49bdd062ada4ca
chk6622 <x1980p@gmail.com>
2019-03-06 00:32:00 +1300

    1.debug. 2.add read file function.

--- src/lab_1/lab_1.py
+++ src/lab_1/lab_1.py
@@ -27,41 +27,41 @@ class TextParser(object):
             taggedS = nltk.pos_tag(nltk.word_tokenize(self.text))
             cp = nltk.RegexpParser(grammar)
             result = cp.parse(taggedS)
-            print(result)
-            result.draw()
+#             print(result)
+#             result.draw()
         return result
-
-#     def processTree(self,tree):
-#         prvWord=''
-#         if tree and isinstance(tree, nltk.tree.Tree):
-#             print(tree)
-#             for node in tree:
-#                 if isinstance(node, nltk.tree.Tree):
-#                     self.processTree(node)
-#                 else:
-#                     if prvWord.upper()=='THE':
-#                         val=self.countMap.get(node[0])
-#                         if not val:
-#                             val=1
-#                         else:
-#                             val=val+1
-#                         self.countMap[node[0]]=val                        
-#                     prvWord=node[0]
+    
+    def parseFile(self,grammar):      
+        result=None
+        if self.filePath:
+            f = open(self.filePath, "r")
+            for line in f:
+                if len(line.strip()):  #if the line is not empty
+                    taggedS = nltk.pos_tag(nltk.word_tokenize(line))
+                    cp = nltk.RegexpParser(grammar)
+                    result = cp.parse(taggedS)
+                    yield result
+        f.close()
    
     def processTree(self,tree):
         
         if tree and isinstance(tree, nltk.tree.Tree):
-            for node in tree:
-                if isinstance(node, nltk.tree.Tree):
-                    self.processTree(node)
-                else:
-                    if node[0].upper()=='THE':
-                        self.buffer.append(node[0])
-                    elif len(self.buffer)>0 and (node[1] in ('JJ','NN','NNP','CD','VBN')):
-                        self.buffer.append(node[0])
-                    else:
+#             print(tree.leaves())
+            for node in tree.leaves():
+                if node[0].upper()=='THE':
+                    if len(self.buffer)>0:
+                            str=(' '.join([item[0] for item in self.buffer])).lower()
+                            val=self.countMap.get(str)
+                            if not val:
+                                val=1
+                            else:
+                                val=val+1
+                            self.countMap[str]=val
+                            self.buffer.clear() 
+                    self.buffer.append(node)
+                elif len(self.buffer)>0 and ('NN' in self.buffer[-1:][0][1]) and ('NN' not in node[1]):#(node[1] in ('JJ','NN','NNP','CD','VBN')):              
                         if len(self.buffer)>0:
-                            str=' '.join(self.buffer)
+                            str=(' '.join([item[0] for item in self.buffer])).lower()
                             val=self.countMap.get(str)
                             if not val:
                                 val=1
@@ -69,6 +69,10 @@ class TextParser(object):
                                 val=val+1
                             self.countMap[str]=val
                             self.buffer.clear()
+                else:
+                    if len(self.buffer)>0:
+                        self.buffer.append(node)
+#             print(self.buffer)
         
     def getCountResult(self):
         aReturn=[item for item in self.countMap.items()]
@@ -77,18 +81,28 @@ class TextParser(object):
     
 
 if __name__ == '__main__':   
-    sent = "The one thing missing in the debate about the proposed World League is any denial that the story broken by the Herald last week is true."
+    sent = '''Vegetarianism is on the rise, as many vegetarians will gladly tell you.
+
+While many people who eschew meat products do so for the sake of animals and the environment, we're starting to learn more about the negative health effects of meat and the benefits from eating a plant-based diet.
+
+We asked five experts if a vegetarian diet is healthier.'''
 #     taggedS = nltk.pos_tag(nltk.word_tokenize(sent))
+    filePath='test.txt'
     grammar = "NP: {<DT>?<JJ>*<NN>}"
 #     cp = nltk.RegexpParser(grammar)
 #     result = cp.parse(taggedS)
 #     print(result)
 #     result.draw()
-    parser=TextParser(text=sent)
-    result=parser.parseText(grammar)
-    parser.processTree(result)
-    print(parser.getCountResult())
-    
+    parser=TextParser(text=sent,filePath=filePath)
+#     result=parser.parseText(grammar)
+#     print(result.leaves())
+    for line in parser.parseFile(grammar):
+        parser.processTree(line)
+    for ind,item in enumerate(parser.getCountResult()):
+        print('%d . %s : %s' % (ind+1,item[0],item[1]))
+#     str='a'
+#     print('d' in str)
+#     print(str[-1:])
 
 
 
